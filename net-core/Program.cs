71a08c16931e5ab45830df1834a7e9f90b4c042a
dedalusmax <rc@ekobit.corp>
2016-12-09 10:27:11 +0100

    removing old mongo provider with toys

--- net-core/Program.cs
+++ net-core/Program.cs
@@ -37,7 +37,6 @@ namespace NetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<DataAccess>();
             services.AddSingleton<IRepository<Transaction>, TransactionRepo>();
             
             services.AddMvc();
