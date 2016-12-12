783bf81bcfd1bc73ea417e25e5ab2951f2c6abba
dedalusmax <dedalus123@gmail.com>
2016-12-12 21:37:20 +0100

    initial checkin of Angular2 app

---
+++ ng2/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { ClarityModule } from 'clarity-angular';
+
+@NgModule({
+  imports:      [ BrowserModule, ClarityModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
