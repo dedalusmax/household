a82f5ffcfe5b415a16a1f30763046010edf7de57
dedalusmax <rc@ekobit.corp>
2017-01-23 08:11:17 +0100

    profile started

--- ng2/app/components/profile.component.ts
+++ ng2/app/components/profile.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Profile } from '../model/profile';
 
 @Component({
   selector: 'profile',
   templateUrl: 'app/templates/profile.component.html'
 })
-export class ProfileComponent { 
+export class ProfileComponent implements OnInit { 
+  model = new Profile();
 
+  constructor (private authService: AuthService) {}
+
+  ngOnInit() {
+    
+  }
 }
